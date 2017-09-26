340942ae7261850a6b2c9548bdbb004849f31735
SeniorDevCode <senior.dev.software@gmail.com>
2017-09-27 01:08:06 +0200

    wizard: connect backend with frontend. Modifications for first release

--- ncp-web/Wizard/JS/wizard.js
+++ ncp-web/Wizard/JS/wizard.js
@@ -1,36 +1,64 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 $(document).ready(function(){
+
+    // launch an request for launch action to the backend
+    function launch_action( action /* string */, args /* object */, next /* callback */ )
+    {
+      $('input').prop('disabled', true);
+      $.post('../ncp-launcher.php',
+        { action:'launch', 
+          ref: action,
+          config: JSON.stringify( args ),
+          csrf_token: document.getElementById( 'csrf-token' ).value
+        } 
+      ).fail( errorMsg ).done( next );
+    }
+
+    function nextTabOnSuccess( data )
+    { 
+      $('input').prop('disabled', false);
+      var res = JSON.parse( data );
+      if ( res.ret && res.ret == 0 )
+        $('#rootwizard').bootstrapWizard('next');
+      else
+        alert( 'error ' + res.output );
+
+      // save next single use token
+      if ( res.token )
+        $('#csrf-token').val( res.token );
+    }
+
+    function show_with_animation( elemid )
+    {
+      $('#' + elemid).show(500);
+      $('html, body').animate({
+        scrollTop: $('#' + elemid).offset().top
+      }, 2000);
+    }
+
+    // Show error on failed AJAX call
+    function errorMsg( data )
+    { 
+      alert('There was an error with the request'); 
+    }
+
 	// This must be first or it breaks
 	$('#rootwizard').bootstrapWizard({onTabShow: function(tab, navigation, index){
-		var $total = navigation.find('li').length;
-		var $current = index+1;
+		var $total = navigation.find('li').length - 1;
+		var $current = index;
 		var $percent = ($current/$total) * 100;
 		$('#rootwizard').find('.progress-bar').css({width:$percent+'%'});
 	}});
-	/* For some reason value="USBStick" didn't work in the <input> element */
-	// document.getElementById('usb-label').value = 'USBStick';
-	// document.getElementById('data-location').value = '/media/USBdrive/ncdata';
-	document.getElementById('freedns-domain').value = 'cloud.ownyourbits.com';
-	document.getElementById('freedns-hash').value = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJK1234567';
-	document.getElementById('freedns-time').value = '10';
-	document.getElementById('noip-user').value = '';
-	document.getElementById('noip-password').value = '';
-	document.getElementById('noip-domain').value = 'mycloud.ownyourbits.com';
-	document.getElementById('noip-time').value = '10';
 
 	// This is required or the tabs aren't styled
 	$('#rootwizard').bootstrapWizard({'tabClass': 'nav nav-pills'}); 
 	// Enable Automount step
 	$('#enable-Automount').on('click', function() {
-		$("#plug-usb").show(500);
-		$('html, body').animate({
-        scrollTop: $("#plug-usb").offset().top
-    }, 2000);
+        show_with_animation( 'plug-usb' );
 		dataTable[0] = {
 			automount: 'yes'
 		};
-		console.log(JSON.stringify(dataTable));
 	});
 	// Disable Automount step
 	$('#disable-Automount').on('click', function() {
@@ -42,123 +70,94 @@ $(document).ready(function(){
 		dataTable[1] = {
 			plugUSB: 'no'
 		};
-		console.log(JSON.stringify(dataTable));
 	});
 	// Enable format-usb step
 	$('#plugUSB').on('click', function() {
-		$("#format-usb").show(500);
-		$('html, body').animate({
-        scrollTop: $("#format-usb").offset().top
-    }, 2000);
 		dataTable[1] = {
 			plugUSB: 'yes'
 		};
-		console.log(JSON.stringify(dataTable));
+
+        launch_action( 'nc-automount', {"ACTIVE":"yes"}, 
+          function ( data )
+          { 
+            $('input').prop('disabled', false);
+            var res = JSON.parse( data );
+            if ( res.ret && res.ret == 0 )         // action ran ok
+              show_with_animation( 'format-usb' );
+            else                                   // action failed
+              alert( 'error: ' + res.output );
+
+            // save next single use token
+            if ( res.token )
+              $('#csrf-token').val( res.token );
+          }
+        );
+
 	});
 	// Enable nextcloud-data step
 	$('#format-USB').on('click', function() {
 		dataTable[2] = {
 			format: 'yes',
 		};
-		$('#rootwizard').bootstrapWizard('next');
-		console.log(JSON.stringify(dataTable));
+
+        launch_action( 'nc-format-USB', {"LABEL":"myCloudDrive"}, 
+          function ( data )
+          { 
+            $('input').prop('disabled', false);
+            var res = JSON.parse( data );
+            if ( res.ret && res.ret == 0 )         // action ran ok
+              show_with_animation( 'nc-datadir-pane' );
+            else                                   // action failed
+              alert( 'error: ' + res.output );
+
+            // save next single use token
+            if ( res.token )
+              $('#csrf-token').val( res.token );
+          }
+        );
 	});
+
 	// Enable nextcloud data tab on skip format.
 	$('#skip-format-USB').on('click', function() {
-		$("#nextcloud-data").show(500);
-		$('html, body').animate({
-			scrollTop: $("#nextcloud-data").offset().top
-    }, 2000);
+        show_with_animation( 'nc-datadir-pane' );
 		dataTable[2] = {
 			format: 'no',
 		};
-		$('#rootwizard').bootstrapWizard('next');
-		console.log(JSON.stringify(dataTable));
 	});
-	// Move to next tab on port forward skip.
-	$('#access-outside-no').on('click', function() {
-		dataTable[4] = {
-			accessOutside: 'no'
+
+	// Launch nc-datadir
+	$('#nc-datadir').on('click', function() {
+		dataTable[2] = {
+			format: 'no',
 		};
-		console.log(JSON.stringify(dataTable));
-		$('#rootwizard').bootstrapWizard('next');
+        launch_action( 'nc-datadir', {"DATADIR":"/media/myCloudDrive/ncdata"}, nextTabOnSuccess );
 	});
-	// Run test ports
-	$('#test-ports-run').on('click', function() {
-		// Run test ports 
-		dataTable[5] = {
-			testPorts: 'yes' // Enable fail2ban
-		};
-		console.log(JSON.stringify(dataTable));
-	});
-	// If test ports are ok, run this
-	$('#test-ports-ok').on('click', function() {
-		$("#port-forward").hide();
-		$("#port-forward-not-ok").hide();
-		dataTable[6] = {
-			testPorts: 'ok'
-		};
-		dataTable[7] = {
-			portForwardRun: 'no'
-		}
-		dataTable[8] = {
-			portForward: 'ok-before'
-		}
-		console.log(JSON.stringify(dataTable));
-		$('#rootwizard').bootstrapWizard('next');
-	});
-	// If test ports fail run this and move to next step
-	$('#test-ports-continue').on('click', function() {
-		$("#port-forward").show(500);
-		$('html, body').animate({
-        scrollTop: $("#port-forward").offset().top
-    }, 2000);
-		dataTable[6] = {
-			testPorts: 'ok'
-		};
-		console.log(JSON.stringify(dataTable));
-	});
+
 	// Run port forwarding with UPnP step
 	$('#port-forward-run').on('click', function() {
 		// Run Port Forwarding and Test Port
 		dataTable[7] = {
 			portForwardRun: 'yes'
 		};
-		console.log(JSON.stringify(dataTable));
+      
+        launch_action( 'nc-forward-ports', {"HTTPSPORT":"443","HTTPPORT":"80"}, nextTabOnSuccess );
 	});
-	// If test after port forwarding is ok, run this
-	$('#port-forward-ok').on('click', function() {
+
+	// Skip port forwarding
+	$('#port-forward-skip').on('click', function() {
 		$("#port-forward-not-ok").hide();
 		dataTable[7] = {
-			portForwardRun: 'yes'
+			portForwardRun: 'no'
 		};
-		dataTable[8] = {
-			portForward: 'ok-after'
-		}
-		console.log(JSON.stringify(dataTable));
 		$('#rootwizard').bootstrapWizard('next');
 	});
+
 	// If test after port forwarding is not ok, run this
 	$('#port-forward-error').on('click', function() {
-		$("#port-forward-not-ok").show(500);
-		$('html, body').animate({
-        scrollTop: $("#port-forward-not-ok").offset().top
-    }, 2000);
+        show_with_animation( 'port-forward-not-ok' );
 		dataTable[8] = {
 			portForward: 'not-ok'
 		};
-		console.log(JSON.stringify(dataTable));
-	});
-	// Enable DDNS step
-	$('#ddns-yes').on('click', function() {
-		$("#choose-ddns").show(500);
-		$('html, body').animate({
-        scrollTop: $("#choose-ddns").offset().top
-    }, 2000);
-		dataTable[9] = {
-			ddns: 'yes'
-		};
-		console.log(JSON.stringify(dataTable));
 	});
 	// Skip DDNS setup
 	$('#ddns-skip').on('click', function() {
@@ -167,21 +166,16 @@ $(document).ready(function(){
 		dataTable[9] = {
 			ddns: 'no'
 		};
-		console.log(JSON.stringify(dataTable));
 	});
 	// Show FreeDNS step
 	$('#ddns-freedns').on('click', function() {
 		$("#noip").hide();
-		$("#freedns").show(500);
-		$('html, body').animate({
-        scrollTop: $("#freedns").offset().top
-    }, 2000);
+        show_with_animation( 'freedns' );
 
 		dataTable[9] = {
 			ddns: 'yes',
 			service: 'freedns'
 		};
-		console.log(JSON.stringify(dataTable));
 	});
 	// Enable FreeDNS step
 	$('#ddns-enable-freedns').on('click', function() {
@@ -190,23 +184,27 @@ $(document).ready(function(){
 			service: 'freedns',
 			domain: $("freedns-domain").val(),
 			updateHash: $("freedns-hash").val(),
-			updateInterval: $("freedns-time").val(),
 		};
-		$('#rootwizard').bootstrapWizard('next');
-		console.log(JSON.stringify(dataTable));
+
+        launch_action( 'freeDNS', 
+          {
+            "ACTIVE":"yes",
+            "UPDATEHASH": $("freedns-hash").val(),
+            "DOMAIN": $("freedns-domain").val(),
+            "UPDATEINTERVAL": "30"
+          },
+          nextTabOnSuccess
+        );
 	});
 	// Show noip step
 	$('#ddns-noip').on('click', function() {
 		$("#freedns").hide();
-		$("#noip").show(500);
-		$('html, body').animate({
-        scrollTop: $("#onip").offset().top
-    }, 2000);
+        show_with_animation( 'noip' );
+
 		dataTable[9] = {
 			ddns: 'yes',
 			service: 'noip'
 		};
-		console.log(JSON.stringify(dataTable));
 	});
 	// Enable noip step
 	$('#ddns-enable-noip').on('click', function() {
@@ -218,11 +216,21 @@ $(document).ready(function(){
 			domain: $("noip-domain").val(),
 			time: $("noip-time").val(),
 		};
-		$('#rootwizard').bootstrapWizard('next');
-		console.log(JSON.stringify(dataTable));
+
+        launch_action( 'no-ip', 
+          {
+            "ACTIVE":"yes",
+            "USER": $("#noip-user").val(),
+            "PASS": $("#noip-password").val(),
+            "DOMAIN": $("noip-domain").val(),
+            "TIME": "30"
+          },
+          nextTabOnSuccess
+        );
 	});
-	
+
+    // click to nextcloud button
+    $('#gotonextcloud').attr('href', window.location.protocol + '//' + window.location.hostname );
 });
 
 var dataTable = [];
-console.log("hi");
